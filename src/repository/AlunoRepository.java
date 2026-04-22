1ae5c5cdcfb816e97cb3110b51b0320bf4b2f414
leonardoIFBA <162935511+leonardoIFBA@users.noreply.github.com>
2026-04-21 21:45:27 -0300

    Refactor AlunoRepository comments and formatting

--- src/repository/AlunoRepository.java
+++ src/repository/AlunoRepository.java
@@ -11,26 +11,31 @@ public class AlunoRepository {
     private static int contadorId = 1;
 
     public AlunoRepository(){
-        // Adicionar alguns cursos para teste
+        // Um aluno precisa de um curso, criamos um curso para vincular com o aluno
         Curso c = new Curso(0, "Teste", 1000);
+        // Adicionar alguns alunos para teste
         alunos.add(new Aluno(contadorId++, "João", 20, "111.222.333.10", c));
         alunos.add(new Aluno(contadorId++, "Maria", 19, "222.333.444.01", c));
         alunos.add(new Aluno(contadorId++, "Bruxa",120, "333.444.555.12", c));
     }
 
+    /**** salva um objeto aluno na lista de alunos */
     public void salvar(Aluno aluno){
         aluno.setId(contadorId++);
         alunos.add(aluno);
     }
 
+    /**** exclui um objeto aluno na lista de alunos */
     public void excluir(Aluno aluno){
         alunos.remove(aluno);
     }
 
+    /**** devolve uma lista de objetos aluno */
     public List<Aluno> listarTodos(){
         return alunos;
     }
 
+    /**** retorna o id */
     public static int getContadorId() {
         return contadorId;
     }
